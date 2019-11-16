6b58fa75ce697fdcc3726587288e27166dc24b73
Andrey Smirnov <iadisey@gmail.com>
2019-11-16 22:17:36 +0200

    was was used scss module

--- src/Page/index.jsx
+++ src/Page/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './index.module.scss'
 
-function App() {
+function Index() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -23,4 +21,5 @@ function App() {
   );
 }
 
-export default App;
+export default Index;
+

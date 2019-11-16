1acbaed47c954fb3b6e35cd76266e091841f700a
Andrey Smirnov <iadisey@gmail.com>
2019-11-17 01:04:02 +0200

    was add scale

--- src/Components/Range/index.jsx
+++ src/Components/Range/index.jsx
@@ -2,13 +2,42 @@ import React from 'react';
 import styles from './styles.module.scss'
 
 function Range(props) {
-    const {value, onChange} = props
-    const change =(event)=>{
-        !!onChange && onChange(event.target.value)
+    const { value, min, max, step, onChange } = props;
+    const change = (event) => {
+        !!onChange && onChange (event.target.value)
+    };
+    const scaleJSX = [];
+    for (let sc=min; sc <= max; sc=sc+step) {
+        scaleJSX.push(<div className={styles.item} key={sc}/>)
     }
     return (
         <div className={styles.range}>
-            <input value={value} type="range" onChange={change}/>
+            <div className={styles.scale}>
+                {scaleJSX}
+            </div>
+            <input
+                value={value}
+                type="range"
+                onChange={change}
+                min={min}
+                max={max}
+                step={step}
+                list="tickmarks"
+                className={styles.input}
+            />
+            <datalist id="tickmarks">
+                <option value="0" label="0%"/>
+                <option value="10"/>
+                <option value="20"/>
+                <option value="30"/>
+                <option value="40"/>
+                <option value="50" label="50%"/>
+                <option value="60"/>
+                <option value="70"/>
+                <option value="80"/>
+                <option value="90"/>
+                <option value="100" label="100%"/>
+            </datalist>
         </div>
     );
 }
@@ -19,5 +48,6 @@ Range.defaultProps = {
     value: 20,
     min: 0,
     max: 100,
+    step: 1,
 };
 
--- src/Page/index.jsx
+++ src/Page/index.jsx
@@ -1,18 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss'
 import { Range } from '../Components'
 
 function Index() {
-    const [val01, setVal01] =useState(50)
+    const [val01, setVal01] = useState (50)
     return (
         <div className={styles.app}>
             <div className={styles.body}>
-                header
+                Range: {val01}
                 <Range
-                value={val01}
-                min={10}
-                max={70}
-                onChange={setVal01}
+                    value={val01}
+                    min={0}
+                    max={100}
+                    step={1}
+                    onChange={setVal01}
+                />
+                <Range
+                    value={val01}
+                    min={0}
+                    max={100}
+                    step={5}
+                    onChange={setVal01}
+                />
+                <Range
+                    value={val01}
+                    min={0}
+                    max={100}
+                    step={10}
+                    onChange={setVal01}
                 />
             </div>
         </div>

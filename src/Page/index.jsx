30cb5d88d69266f2f0875cebedc55066652c089b
Andrey Smirnov <iadisey@gmail.com>
2019-11-21 01:43:18 +0200

    was start add radioButtons

--- src/Page/index.jsx
+++ src/Page/index.jsx
@@ -3,108 +3,106 @@ import styles from './styles.module.scss'
 import { Range, Test, RadioButtonsExamples } from '../Components'
 
 function Index() {
-    const [val01, setVal01] = useState (40);
+    const [count, setCount] = useState (40);
     return (
-        <div className={ styles.app }>
-            <div className={ styles.body }>
-                <div className={ styles.container }>
-                    <button onClick={ () => setCount(count + 1) }>MAIN +</button>
-                    MAIN Count: { count }
-                </div>
-                <div className={ styles.container }>
-                    <Test/>
-                </div>
-                <div className={ styles.container }>
-                    <button onClick={ () => setVal01(val01 + 1) }>Value +</button>
-                    Range: { val01 }
-                </div>
-                <div className={ styles.container }>
-                    <Range
-                        value={ val01 }
-                        min={ 0 }
-                        max={ 100 }
-                        step={ 1 }
-                        marker={ 10 }
-                        onChange={ setVal01 }
-                    />
-                </div>
-                <div className={ styles.container }>
-                    <Range
-                        value={ val01 }
-                        min={ 0 }
-                        max={ 100 }
-                        step={ 2 }
-                        marker={ 10 }
-                        onChange={ setVal01 }
-                    />
-                </div>
-                <div className={ styles.container }>
-                    <Range
-                        value={ val01 }
-                        min={ 0 }
-                        max={ 100 }
-                        step={ 5 }
-                        marker={ 25 }
-                        onChange={ setVal01 }
-                        suffix={ '%' }
-                    />
-                </div>
-                <div className={ styles.container }>
-                    <Range
-                        value={ val01 }
-                        min={ 0 }
-                        max={ 70 }
-                        step={ 2 }
-                        marker={ 10 }
-                        onChange={ setVal01 }
-                        title={ 'Водка:' }
-                        suffix={ 'º' }
-                    />
-                </div>
-                <Range
-                    value={ val01 }
-                    min={ 0 }
-                    max={ 73 }
-                    step={ 3 }
-                    marker={ 11 }
-                    onChange={ setVal01 }
-                />
-                <Range
-                    value={ val01 }
-                    min={ 0 }
-                    max={ 1800 }
-                    step={ 10 }
-                    marker={ 100 }
-                    onChange={ setVal01 }
-                />
         <div className={styles.app}>
             <div className={styles.body}>
                 <div className={styles.container}>
-                    Range: {val01}
+                    <button onClick={() => setCount (count + 1)}>MAIN +</button>
+                    MAIN Count: {count}
+                </div>
+                <div className={styles.container}>
+                    <Test/>
+                </div>
+                <div className={styles.container}>
+                    <button onClick={() => setCount (count + 1)}>Value +</button>
+                    Range: {count}
+                </div>
+                <div className={styles.container}>
                     <Range
-                        value={val01}
+                        value={count}
                         min={0}
                         max={100}
                         step={1}
                         marker={10}
-                        onChange={setVal01}
+                        onChange={setCount}
                     />
+                </div>
+                <div className={styles.container}>
                     <Range
-                        value={val01}
+                        value={count}
+                        min={0}
+                        max={100}
+                        step={2}
+                        marker={10}
+                        onChange={setCount}
+                    />
+                </div>
+                <div className={styles.container}>
+                    <Range
+                        value={count}
                         min={0}
                         max={100}
                         step={5}
                         marker={25}
-                        onChange={setVal01}
+                        onChange={setCount}
                         suffix={'%'}
                     />
+                </div>
+                <div className={styles.container}>
                     <Range
-                        value={val01}
+                        value={count}
                         min={0}
                         max={70}
                         step={2}
                         marker={10}
-                        onChange={setVal01}
+                        onChange={setCount}
+                        title={'Водка:'}
+                        suffix={'º'}
+                    />
+                </div>
+                <Range
+                    value={count}
+                    min={0}
+                    max={73}
+                    step={3}
+                    marker={11}
+                    onChange={setCount}
+                />
+                <Range
+                    value={count}
+                    min={0}
+                    max={1800}
+                    step={10}
+                    marker={100}
+                    onChange={setCount}
+                />
+                <div className={styles.container}>
+                    Range: {count}
+                    <Range
+                        value={count}
+                        min={0}
+                        max={100}
+                        step={1}
+                        marker={10}
+                        onChange={setCount}
+                    />
+                    <Range
+                        value={count}
+                        min={0}
+                        max={100}
+                        step={5}
+                        marker={25}
+                        onChange={setCount}
+                        suffix={'%'}
+                    />
+                    <Range
+                        value={count}
+                        min={0}
+                        max={70}
+                        step={2}
+                        marker={10}
+                        onChange={setCount}
                         title={'Водка:'}
                         suffix={'º'}
                     />
@@ -119,4 +117,3 @@ function Index() {
 }
 
 export default Index;
-
